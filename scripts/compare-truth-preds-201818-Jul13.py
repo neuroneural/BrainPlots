90d95c5914702f5e842898806c280ad12c4006c5
Linlin Lu <pku600@126.com>
2023-07-31 18:02:46 -0400

    add comment

--- scripts/compare-truth-preds-201818-Jul13.py
+++ scripts/compare-truth-preds-201818-Jul13.py
@@ -1,38 +1,31 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
+# Importing necessary libraries
 import os
-
-
 import pyvista as pv
 from pyvista import examples
-
-
 from collections import defaultdict
 
+# Setting pyvista global theme to have a transparent background
 pv.global_theme.transparent_background = True
 
-
-# In[5]:
-
-
+# Setting up the directory of the data and the models' names
 pre = '/Users/mialu/Documents/Course22Fall/TReNDS/neuroneural/BrainPlots/data/201818/all201818'
 models1 = ['cortexode', 'corticalflow','deepcsr','pialnn','vox2cortex']
 models2 = ['cortexode', 'corticalflow','deepcsr','topofit','vox2cortex']
 
-
-# In[6]:
-
-
+# Function to organize files
 def org_files(surf, models, pre = pre):
     preds = []
     gts = []
+    # Loop through each model
     for m in models:
+        # Loop through each file in the directory
         for f in os.listdir(pre):
+            # Checking if the file belongs to the model and surface 
             if m in f and surf in f:
+                # Special handling for certain models
                 if m in ['corticalflow', 'deepcsr', 'vox2cortex']:
                     if 'transformed' in f and '-rm-' in f:
                         if 'gt' not in f:
@@ -47,22 +40,22 @@ def org_files(surf, models, pre = pre):
                             gts.append(os.path.join(pre,f))
     return preds, gts
 
+# Calling the function for 'pial' surface and models1
 surf = 'pial'
 models = models1
 preds1, gts1 = org_files(surf,models)
 
+# Calling the function for 'white' surface and models2
 surf = 'white'
 models = models2
 preds2, gts2 = org_files(surf,models)
 
-
-# In[7]:
-
-
+# Create the plotter object
 plotter = pv.Plotter(shape=(2, len(models1)))
 
 files1,file_truths1, files2, file_truths2 = preds1, gts1, preds2, gts2
 
+# Adding the models to the first row of subplots
 for idx in range(len(models1)):
     mesh1, mesh2 = pv.read(files1[idx]), pv.read(file_truths1[idx])
     plotter.subplot(0, idx)
@@ -70,7 +63,7 @@ for idx in range(len(models1)):
     plotter.add_mesh(mesh1)
     plotter.add_mesh(mesh2, color='b', opacity=0.7)  
 
-
+# Adding the models to the second row of subplots
 for idx in range(len(models1)):
     mesh1, mesh2 = pv.read(files2[idx]), pv.read(file_truths2[idx])
     plotter.subplot(1, idx)
@@ -78,10 +71,11 @@ for idx in range(len(models1)):
     plotter.add_mesh(mesh1)
     plotter.add_mesh(mesh2, color = 'b', opacity=0.7)   
 
-    
 # Link the camera position of all subplots
 plotter.link_views()
+
 # Render the plot
 print('Grey is predicted pial. \nBlue is their respective ground truth. \n\n1st row: transformed [vox2cortex,deepcsr,corticalflow]. \n\n2nd row, the 3 are not transformed.')
 plotter.show()
+
 
