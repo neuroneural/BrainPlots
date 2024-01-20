1d5ebf901d944315ce283a4b54b15a27dc6b20a7
William Ashbee <wsashbee@gmail.com>
2024-01-20 20:39:00 +0000

    very interesting white-pial intersections

--- scripts/medialwallv2/deepcsr/wpintplot.py
+++ scripts/medialwallv2/deepcsr/wpintplot.py
@@ -8,12 +8,19 @@ outliers_df = pd.read_csv('mw_outliers.csv')
 excluded_subject_ids = outliers_df['subject_id'].unique()
 
 # Load the main data
-df = pd.read_csv('distances.csv')
+df = pd.read_csv('white_pial_intersections.csv')
 
 # Filter out the outliers based on subject_ids
 df = df[~df['subject_id'].isin(excluded_subject_ids)]
 
+# Print the number of rows used in the plot
 print("Number of rows used in the plot:", df.shape[0])
+
+# Extract the top 5 Intersections White-Pial, subject_ids, Hemisphere
+top_5 = df.sort_values(by='Intersections White-Pial', ascending=False).head(5)
+
+# Write the top 5 to a CSV file
+top_5.to_csv('top_5_intersections_white_pial.csv', index=False)
 
 # Set global font size using rcParams
 mpl.rcParams.update({'font.size': 16})  # Adjust this value as needed
@@ -22,11 +29,11 @@ mpl.rcParams.update({'font.size': 16})  # Adjust this value as needed
 subplot_height = 5  # Adjust height
 subplot_aspect = 1.2  # Adjust aspect ratio
 
-# Create a FacetGrid for the subplots
-g = sns.FacetGrid(df, col='Hemisphere', row='Surface Type', sharex=True, sharey=False, height=subplot_height, aspect=subplot_aspect)
+# Create a FacetGrid for the subplots, one for each Hemisphere
+g = sns.FacetGrid(df, col='Hemisphere', sharex=True, sharey=False, height=subplot_height, aspect=subplot_aspect)
 
-# Plot using strip plot for 'Self-Intersection C_mwrm' to add jitter
-g.map_dataframe(sns.stripplot, x='Project', y='Self-Intersection C_mwrm', jitter=0.05, dodge=True)
+# Plot using strip plot for 'Intersections White-Pial' to add jitter
+g.map_dataframe(sns.stripplot, x='Project', y='Intersections White-Pial', jitter=0.05, dodge=True)
 
 # Adjust the layout
 g.set_xticklabels(rotation=15, ha='right')  # Adjusting tick labels for all subplots
@@ -34,11 +41,11 @@ g.set_xticklabels(rotation=15, ha='right')  # Adjusting tick labels for all subp
 # Adjusting subplot titles, axis labels, and font size
 for ax in g.axes.flatten():
     ax.set_title(ax.get_title(), fontsize=14)  # Adjust title font size
-    ax.set_ylabel('Self Intersections', fontsize=14)  # Set y-axis label and font size
-    ax.set_xlabel(ax.get_xlabel(), fontsize=14)  # Set x-axis label and font size
+    ax.set_ylabel('Intersections White-Pial', fontsize=14)  # Set y-axis label and font size
+    ax.set_xlabel('Project', fontsize=14)  # Set x-axis label and font size
 
 plt.tight_layout()
 
 # Save or display the plot
-plt.savefig('self_intersection_scatter_plot.png')
+plt.savefig('white_pial_intersections_scatter_plot.png')
 # plt.show()  # Use this instead of savefig if you want to display the plot directly
